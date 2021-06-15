30a4c7536fe4b994c6aa40e0bdbcf232909d18e1
luiseduve <luiseduve@hotmail.com>
2021-06-15 13:44:08 +0200

    Fix plot rolling ts, and add feature extrc summary stats

--- plotter/ts_visualization.py
+++ plotter/ts_visualization.py
@@ -163,8 +163,8 @@ def plot_mean_std_ts(data:np.ndarray, data_labels:np.ndarray, rolling_window_siz
 
             # Moving window with average to reduce noise.
             # Calculates mean and std among all time series for the specific class
-            mean_ts = data_temp.rolling(rolling_window_size).mean().mean()
-            sd_ts   = data_temp.rolling(rolling_window_size).mean().std()
+            mean_ts = data_temp.rolling(rolling_window_size, axis=1).mean().mean()
+            sd_ts   = data_temp.rolling(rolling_window_size, axis=1).mean().std()
 
             # Limits to fill are in the plot
             low_line  = (mean_ts - sd_ts)
